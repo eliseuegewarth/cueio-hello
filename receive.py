5320d1de3cd85b6d5d575d64c6cbf92876be5037
Eliseu Egewarth <eliseuegewarth@gmail.com>
2018-09-13 16:48:03 -0300

    Adding receive.py file
    Signed-off-by: Eliseu Egewarth <eliseuegewarth@gmail.com>

---
+++ receive.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import pika
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+
+if __name__ == '__main__':
+
+    # Connect to rabbitmq
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Declare a queue named 'hello'
+    channel.queue_declare(queue='hello')
+
+
+    channel.basic_consume(callback,
+                          queue='hello',
+                          no_ack=True)
+
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
